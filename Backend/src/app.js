3cc9bc36a7e765aa21268cce6661b1e436fa96a5
Bhumika <bhumikaa9873@gmail.com>
2026-05-17 06:49:00 +0530

    updated cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ const path = require('path');
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://food-stream.vercel.app/',
     credentials: true,
 }));
 
