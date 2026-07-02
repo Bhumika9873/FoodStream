30dce7483b67353a8d6ea8cad23de26263fd227d
Bhumika <bhumikaa9873@gmail.com>
2026-07-02 17:47:56 -0500

    updating food partner details

--- Backend/src/controllers/food-partner.controller.js
+++ Backend/src/controllers/food-partner.controller.js
@@ -2,24 +2,41 @@ const foodPartnerModel = require('../models/foodpartner.model');
 const foodModel = require('../models/food.model');
 
 async function getFoodPartnerById(req, res) {
+    try {
 
-    const foodPartnerId = req.params.id;
+        const foodPartnerId = req.params.id;
 
-    const foodPartner = await foodPartnerModel.findById(foodPartnerId)
-    const foodItemsByFoodPartner = await foodModel.find({ foodPartner: foodPartnerId })
+        const foodPartner = await foodPartnerModel.findById(foodPartnerId);
 
-    if (!foodPartner) {
-        return res.status(404).json({ message: "Food partner not found" });
-    }
-
-    res.status(200).json({
-        message: "Food partner retrieved successfully",
-        foodPartner: {
-            ...foodPartner.toObject(),
-            foodItems: foodItemsByFoodPartner
+        if (!foodPartner) {
+            return res.status(404).json({
+                message: "Food partner not found"
+            });
         }
 
-    });
+        const foodItemsByFoodPartner = await foodModel.find({
+            foodPartner: foodPartnerId
+        });
+
+        const totalMeals = foodItemsByFoodPartner.length;
+
+        res.status(200).json({
+            message: "Food partner retrieved successfully",
+            foodPartner: {
+                ...foodPartner.toObject(),
+                totalMeals: totalMeals,
+                customersServed: foodPartner.customersServed || 0,
+                foodItems: foodItemsByFoodPartner
+            }
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: error.message
+        });
+    }
 }
 
 module.exports = {
